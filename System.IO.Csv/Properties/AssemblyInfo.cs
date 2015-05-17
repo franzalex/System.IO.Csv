3cdab4ef33a4b114d96c1e563866acb2e1e73666
Franz Alex Gaisie-Essilfie <falexge@gmail.com>
2015-05-17 16:44:26 +0000

    Updated assembly description in AssemblyInfo.cs

--- System.IO.Csv/Properties/AssemblyInfo.cs
+++ System.IO.Csv/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("System.IO.Csv")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Handles reading and writing CSV files.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("System.IO.Csv")]
